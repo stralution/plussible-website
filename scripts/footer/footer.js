ea9e403c388395484cae098b7fc2b4cb018c8257
stralution <stralution@gmail.com>
2025-07-04 03:31:03 -0500

    Delete scripts directory

--- scripts/footer/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('footer.html')
-      .then(response => response.text())
-      .then(data => {
-          document.querySelector("footer").innerHTML = data;
-      })
-      .catch(error => console.error('Error loading footer:', error));
-});
